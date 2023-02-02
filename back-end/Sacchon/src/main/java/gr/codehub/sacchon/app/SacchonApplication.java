58af0d7feb3569795eb0ecc0e5bd857060229228
ntagky <ntagkonik@csd.auth.gr>
2023-02-02 21:03:27 +0200

    Create backend branch & empty project

---
+++ back-end/Sacchon/src/main/java/gr/codehub/sacchon/app/SacchonApplication.java
@@ -0,0 +1,11 @@
+package gr.codehub.sacchon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacchonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SacchonApplication.class);
+    }
+}
