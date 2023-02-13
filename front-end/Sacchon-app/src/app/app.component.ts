af1f4e53f4a09ab2453bf5049162f501c5a96dc1
georgia <giannokosta@net2grid.com>
2023-02-13 22:50:34 +0200

    Initial commit

---
+++ front-end/Sacchon-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Sacchon-app';
+  team = 'Team 1';
+}
