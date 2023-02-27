b2f68ba58660923d9e0a8fe0b84964e1d8805e5c
BoxingBox <c.tzoulias@gmail.com>
2023-02-27 20:47:30 +0200

    Added profile and delete components

---
+++ front-end/Sacchon-app/src/app/delete-button/delete-button.component.ts
@@ -0,0 +1,36 @@
+import { DeleteService } from './../services/delete.service';
+import { Component,OnInit } from '@angular/core';
+import { InfoService } from '../services/info.service';
+import {RouterModule} from  '@angular/router';
+
+@Component({
+  selector: 'app-delete-button',
+  templateUrl: './delete-button.component.html',
+  styleUrls: ['./delete-button.component.scss']
+})
+export class DeleteButtonComponent implements OnInit {
+
+  data:any;
+  response:any;
+
+  constructor(private deleteService: DeleteService ){}
+
+  ngOnInit(): void {
+    this.deleteData();
+
+  }
+
+  deleteData(){
+
+
+
+    this.deleteService.delete().subscribe(
+   {
+      next: data =>this.response = data,
+
+   }
+
+
+ )}
+
+}
