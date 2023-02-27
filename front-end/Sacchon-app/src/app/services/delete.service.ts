1d5ce35d59ab71198b46a410d03d754498209439
BoxingBox <c.tzoulias@gmail.com>
2023-02-27 22:00:15 +0200

    Fix Delete Button

--- front-end/Sacchon-app/src/app/services/delete.service.ts
+++ front-end/Sacchon-app/src/app/services/delete.service.ts
@@ -5,14 +5,13 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class DeleteService {
-
-
 
   constructor(private http:HttpClient) { }
 
   delete(){
-
     return this.http.delete('http://localhost:9000/delete/2')
   }
 }
