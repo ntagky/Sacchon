2313f1cadfc8b38a4281355eec1d22eddeb8ff5a
BoxingBox <c.tzoulias@gmail.com>
2023-02-28 20:40:49 +0200

    Minor PortalController fix

--- back-end/Sacchon/src/main/java/gr/codehub/sacchon/app/controller/PortalController.java
+++ back-end/Sacchon/src/main/java/gr/codehub/sacchon/app/controller/PortalController.java
@@ -1,7 +1,6 @@
 package gr.codehub.sacchon.app.controller;
 
 import gr.codehub.sacchon.app.dto.PortalDto;
-import gr.codehub.sacchon.app.model.AESUtil;
 import gr.codehub.sacchon.app.service.PortalService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +17,7 @@ public class PortalController {
 
     @GetMapping("/login")
     public PortalDto findUser(@RequestParam("email") String email, @RequestParam("password") String password){
-        System.out.println("Password give: " + password + " and decrypted:" + new AESUtil().decrypt(password, password));
-        return portalService.findRegisteredUser(email.replace("%", "&"),  new AESUtil().decrypt(password, password));
+        return portalService.findRegisteredUser(email,password);
     }
 
 }
