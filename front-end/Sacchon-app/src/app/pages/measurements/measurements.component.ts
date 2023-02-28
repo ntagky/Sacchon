cd5780c4f73076d0b7435ff34df9a32ec4edf846
georgia <giannokosta@net2grid.com>
2023-02-28 21:59:57 +0200

    Remove hardcoded data v1

--- front-end/Sacchon-app/src/app/pages/measurements/measurements.component.ts
+++ front-end/Sacchon-app/src/app/pages/measurements/measurements.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-measurements',
-  templateUrl: './pages/measurements.component.html',
-  styleUrls: ['./pages/measurements.component.scss']
+  templateUrl: './measurements.component.html',
+  styleUrls: ['./measurements.component.scss']
 })
 export class MeasurementsComponent {
 
